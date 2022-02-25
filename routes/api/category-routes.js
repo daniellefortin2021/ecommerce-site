8c318de4209269603b72b1ea7dafee14873eada1
Danielle Fortin <fortind6@gmail.com>
2022-02-25 16:29:05 -0500

    category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,67 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    //gets book based on id 
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
