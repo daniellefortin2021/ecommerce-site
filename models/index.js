aa4647dec6a9d85240a599b5452ffb363265f955
Danielle Fortin <fortind6@gmail.com>
2022-02-26 12:39:41 -0500

    everything works

--- models/index.js
+++ models/index.js
@@ -11,7 +11,8 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: '"category_id'
+  foreignKey: 'category_id',
+  
 });
 
 // Products belongToMany Tags (through ProductTag)
@@ -24,7 +25,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: "tag_id",
-  onDelete: "CASCADE",
+  // onDelete: "CASCADE"
 })
 
 module.exports = {
