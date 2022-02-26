bc8d68f2044aabf591e195f9059bc1c7374f113a
Danielle Fortin <fortind6@gmail.com>
2022-02-26 11:45:49 -0500

    product routes are not working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,24 +8,20 @@ router.get('/', (req, res) => {
   // find all products
   Product.findAll({
     // be sure to include its associated Category and Tag data
-    include: [
-
-      { model: Category }, { model: Tag }
-    ],
-    })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+    include: [{ model: Category }, { model: Tag }],
   })
-  
+  .then((dbPostData) => res.json(dbPostData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   Product.findOne({
-    // be sure to include its associated Category and Tag data
+        // be sure to include its associated Category and Tag data
     include: [
       { model: Category }, { model: Tag }
     ],
